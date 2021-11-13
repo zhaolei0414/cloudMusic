51bd07978e9b5a3503aba015c1584355e9a4c925
赵磊 <1403665068@qq.com>
2021-11-13 22:20:21 +0800

    优化

--- src/utils/changeValue.js
+++ src/utils/changeValue.js
@@ -2,8 +2,13 @@ export const changeValue = (value) => {
   if (typeof value === 'string') {
     value = +value
   }
+  if (value >= 100000000) {
+    value = Math.floor(value / 100000000) + '亿'
+    return value
+  }
   if (value >= 10000) {
     value = Math.floor(value / 10000) + '万'
+    return value
   }
   return value
 }
